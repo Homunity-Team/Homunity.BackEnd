1b94e876347750e6e1d56a4ce3731583644a3afc
Shenoda Mohsen <shenodamohsen122@gmail.com>
2026-01-19 12:53:48 +0200

    Modify File Program.cs

--- Homunity Web_Api/Program.cs
+++ Homunity Web_Api/Program.cs
@@ -37,7 +37,7 @@ app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Homunity API v1");
-    c.RoutePrefix = string.Empty; // <-- ?? ?????
+    //  c.RoutePrefix = string.Empty; // <-- ?? ?????
 });
 
 
